271281c548dd5ef57ba2845bc916a695e47ed038
Dennis Wachira <dirungu707@gmail.com>
2024-11-12 09:20:22 +0300

    initial commit

--- src/ast/lexer.rs
+++ src/ast/lexer.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 #[derive(PartialEq,Eq,Clone,Debug)]
 pub struct TextSpan{
      start: usize,
@@ -53,65 +55,91 @@ impl<'a> Lexer<'a>{
 
    
   pub  fn next_token(&mut self)->Option<Token>{
-        if self.current_position > self.input.len(){
-            return None;
-        }
+        // if self.current_position > self.input.len(){
+        //     return None;
+        // }
 
         if self.current_position == self.input.len(){
             let eof_char = '\0';
             let eof_token_kind = TokenKind::Eof;
             let span = TextSpan::new(0, 0, eof_char.to_string());
+            self.current_position+=1;
             return Some(Token::new(eof_token_kind, span));
 
         }
         let start = self.current_position;
         let c = self.current_char();
-        
-        let mut kind = TokenKind::Bad;
+        return c.map(|c|{
+       let mut kind = TokenKind::Bad;
         if self.is_number_start(&c){
             let number = self.consume_number();
             kind = TokenKind::Number(number);
             
 
+        }else if self.is_binary_operator(&c) {
+            kind = self.consume_operator().unwrap();
+        
+            println!("{:?}",kind);
+        }else{
+            kind = TokenKind::Bad;
         }
         let end = self.current_position;
         let literal = c.to_string();
         let span = TextSpan::new(start, end, literal);
         let token = Token::new(kind,span);
-        return Some(token);
+        return token;
+        });
+        
         
     }
     fn consume(&mut self)->Option<char>{
-        let current_char = self.current_char();
-        self.current_position += 1;
         if self.current_position > self.input.len(){
             return None;
         }
+        let current_char = self.current_char()?;
+        self.current_position += 1;
         return Some(current_char);
     }
     fn consume_number(&mut self)->i64{
         let mut number:i64 = 0;
-        while let Some(c) =self.consume(){
+        let c =self.consume().unwrap();
+        
+           
             if c.is_digit(10){
+            
              number = number * 10 + c.to_digit(10).unwrap() as i64;
              
-            }else{
-                break;
             }
-        }
+        
         return number;
        
     }
-fn is_number_start(&self,c:&char)->bool{
+    fn consume_operator(&mut self)->Option<TokenKind>{
+        let c = self.consume().unwrap();
+        return match c{
+            '+' =>Some(TokenKind::Plus),
+            '-' =>Some(TokenKind::Minus),
+            '*' => Some(TokenKind::Asterisk),
+            '/' => Some(TokenKind::Slash),
+            _ => Some(TokenKind::Bad)
+        };
+    }
+    fn is_number_start(&self,c:&char)->bool{
         c.is_digit(10)
     }
+    fn is_binary_operator(&self,c:&char)->bool{
+        let operators: HashSet<char> = ['+', '-', '*', '/', '%', '&', '|'].iter().cloned().collect();
+        operators.contains(c)
+    }
 
-    
+    // fn peek_char(&self)->Option<char>{
+    //     self.input.chars().nth(self.current_position + 1)
+    // }
     
     
 
-    fn current_char(&self)->char{
-        return self.input.chars().nth(self.current_position).unwrap_or('\0');
+    fn current_char(&self)->Option<char>{
+        return self.input.chars().nth(self.current_position);
     }
     
 
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod ast;
 
 
 fn main() {
-     let input:&str = "7";
+     let input:&str = "7+";
     let mut lexer =Lexer::new(input);
     let mut tokens = Vec::new();
     while let Some(token) = lexer.next_token(){
